ac04be4bb6c7cca65ceefb38f69f24dfe5c81f9c
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2021-01-30 11:28:10 -0800

    Drop a todo.

--- src/chronicle_leader.erl
+++ src/chronicle_leader.erl
@@ -13,7 +13,6 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%
-%% TODO: incorporate priorities?
 -module(chronicle_leader).
 
 -include("chronicle.hrl").
