90592c4e5cea67a355f380d2be3b817736e422d1
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2021-04-23 10:00:18 -0700

    Allow dumping snapshots with --raw flag.

--- scripts/chronicle_dump/chronicle_dump.erl
+++ scripts/chronicle_dump/chronicle_dump.erl
@@ -6,28 +6,70 @@
 -export([raw/1]).
 
 -define(fmt(Msg), ?fmt(Msg, [])).
--define(fmt(Fmt, Args), io:format(Fmt "~n", Args)).
+-define(fmt(Fmt, Args),
+        begin
+            io:format(Fmt, Args),
+            io:nl()
+        end).
 
 -define(RAW_TAG, '$chronicle_dump_raw').
 
 raw(Term) ->
     {?RAW_TAG, Term}.
 
-dump_snapshots([]) ->
-    ok;
-dump_snapshots([Path]) ->
-    dump_snapshot(Path);
-dump_snapshots([Path|Rest]) ->
-    dump_snapshot(Path),
-    ?fmt("~n"),
-    dump_snapshots(Rest).
+parse_args(Args, Spec0) ->
+    Spec = maps:from_list(
+             lists:map(
+               fun ({Opt, Type}) ->
+                       {atom_to_list(Opt), Type}
+               end, maps:to_list(Spec0))),
+    parse_args_loop(Args, [], #{}, Spec).
 
-dump_snapshot(Path) ->
+parse_args_loop([], AccArgs, AccOptions, _Spec) ->
+    {lists:reverse(AccArgs), AccOptions};
+parse_args_loop([Arg|Args], AccArgs, AccOptions, Spec) ->
+    case Arg of
+        "--" ->
+            {lists:reverse(AccArgs, Args), AccOptions};
+        "--" ++ Option ->
+            case maps:find(Option, Spec) of
+                {ok, flag} ->
+                    Opt = list_to_atom(Option),
+                    parse_args_loop(Args, AccArgs,
+                                    AccOptions#{Opt => true},
+                                    Spec);
+                error ->
+                    usage("unknown option '~s'", [Arg])
+            end;
+        _ ->
+            parse_args_loop(Args, [Arg | AccArgs], AccOptions, Spec)
+    end.
+
+dump_snapshots(Args) ->
+    {Paths, Options} = parse_args(Args, #{raw => flag}),
+    dump_snapshots_loop(Paths, Options).
+
+dump_snapshots_loop([], _) ->
+    ok;
+dump_snapshots_loop([Path], Options) ->
+    dump_snapshot(Path, Options);
+dump_snapshots_loop([Path|Rest], Options) ->
+    dump_snapshot(Path, Options),
+    ?fmt("~n"),
+    dump_snapshots_loop(Rest, Options).
+
+dump_snapshot(Path, Options) ->
     case chronicle_storage:read_rsm_snapshot(Path) of
         {ok, Snapshot} ->
-            Props = chronicle_rsm:format_snapshot(Snapshot),
             ?fmt("Dumping '~s'~n", [Path]),
-            dump_props(Props);
+
+            case maps:get(raw, Options, false) of
+                true ->
+                    dump_term(Snapshot);
+                false ->
+                    Props = chronicle_rsm:format_snapshot(Snapshot),
+                    dump_props(Props)
+            end;
         {error, Error} ->
             ?fmt("Couldn't read snapshot '~s': ~w", [Path, Error])
     end.
@@ -56,6 +98,9 @@ dump_elem(Indent, Elem) ->
 
 dump_string(Indent, String) ->
     ?fmt("~s~s", [Indent, String]).
+
+dump_term(Term) ->
+    dump_term("", Term).
 
 dump_term(Indent, Term) ->
     ?fmt("~s~250p", [Indent, Term]).
@@ -110,12 +155,17 @@ type(Term) ->
 usage() ->
     erlang:halt(1).
 
+-spec usage(Fmt::io:format(), Args::[any()]) -> no_return().
+usage(Fmt, Args) ->
+    ?fmt(Fmt, Args),
+    usage().
+
 main(Args) ->
     persistent_term:put(?CHRONICLE_LOAD_NIFS, false),
 
     case Args of
-        ["snapshot" | Paths] ->
-            dump_snapshots(Paths);
+        ["snapshot" | RestArgs] ->
+            dump_snapshots(RestArgs);
         _ ->
             usage()
     end.
